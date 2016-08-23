2d3e105869b67689ed5d0848e330c18e5046df46
joe <joeplonsker@gmail.com>
2016-08-23 13:58:38 -0400

    Add review question 4

--- 1chpt/1chptReview.java
+++ 1chpt/1chptReview.java
@@ -15,3 +15,26 @@ class Leaf {
 The code does not contain any compiler errors. It is valid to define multiple classes in a single file as long as only one of them is public and the others have default access
 
 */
+
+
+// --------------------
+
+/* 2. Suppose a class named com.mycompany.Main is a Java application, and Main.class is in the following directory:
+
+\projects\build\com\mycompany
+
+Here are commands that successfully executes Main.
+
+java -classpath \projects\build com.mycompany.Main
+
+java -cp \projects\build com.mycompany.Main
+
+*/
+
+// --------------------
+
+
+
+
+
+
